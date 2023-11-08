d59bf47ff1369f8c3c8ae6d163357c24b7d83a23
TristanStier <stier.tristan@gmail.com>
2023-11-08 17:22:14 -0500

    Multiscript Communication
    I got the popup.js sending messages to content.js but for some reason it only works on certain websites.

--- content.js
+++ content.js
@@ -1,15 +1,43 @@
-// Listen for messages from the popup
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === 'sendMessage') {
-        const message = request.message;
-        
-        // Log the received message to the console for testing
-        console.log(`Message from popup: ${message}`);
+// // Function to add a message to the chatbox
+// function addMessageToChatbox(message, isUserMessage) 
+// {
+//     const chatMessages = document.getElementById('chat-messages');
+//     const messageElement = document.createElement('p');
+//     messageElement.innerText = message;
 
-        // Display the received message in the web page
-        const chatMessages = document.getElementById('chat-messages'); // Assuming your chatbox has an element with this ID
-        const userMessage = document.createElement('p');
-        userMessage.innerText = `You: ${message}`;
-        chatMessages.appendChild(userMessage);
+//     if (isUserMessage) 
+//     {
+//         messageElement.classList.add('user-message');
+//     } 
+//     else 
+//     {
+//         messageElement.classList.add('echo-message');
+//     }
+
+//     chatMessages.appendChild(messageElement);
+// }
+
+// chrome.runtime.onMessage.addListener
+// (
+//     function(request, sender, sendResponse) 
+//     {
+//         if (request.message === 'sendMessage') 
+//         {
+//             const message = request.message;
+//             addMessageToChatbox(message, true);
+//             addMessageToChatbox(message, false);
+//         }
+//     }
+// );
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "start" ) {
+            start();
+        }
     }
-});
+);
+
+function start(){
+    alert("started");
+}
--- popup.js
+++ popup.js
@@ -1,42 +1,56 @@
-// Function to send a message to the content script
-function sendMessageToContentScript(message) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError);
-            return;
-        }
+// // Function to send a message to the content script
+// function sendMessageToContentScript(message) 
+// {
+//     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) 
+//     {
+//         if (chrome.runtime.lastError) 
+//         {
+//             console.error(chrome.runtime.lastError);
+//             return;
+//         }
 
-        const activeTab = tabs[0];
+//         const activeTab = tabs[0];
 
-        if (activeTab) {
-            chrome.scripting.executeScript({
-                target: { tabId: activeTab.id },
-                function: (message) => {
-                    // Send a message to the content script
-                    chrome.runtime.sendMessage({ action: 'sendMessage', message });
-                },
-                args: [message],
-            });
-        } else {
-            console.error("No active tab found.");
-        }
+//         if (activeTab) 
+//         {
+//             chrome.scripting.executeScript
+//             ({
+//                 target: { tabId: activeTab.id },
+//                 function:(message) => 
+//                 {
+//                     // Send a message to the content script
+//                     chrome.tabs.sendMessage(activeTab.id, {"message": "sendMessage"});
+//                 },
+//                 args:[message],
+//             });
+//         }
+//         else 
+//         {
+//             console.error("No active tab found.");
+//         }
+//     });
+// }
+
+// // Handle the send button click event
+// document.getElementById('send-button').addEventListener('click', function () 
+// {
+    // const userInput = document.getElementById('user-input');
+    // const message = userInput.value;
+
+    // if (message) 
+    // {
+    //     sendMessageToContentScript(message);
+    //     userInput.value = '';
+    // }
+// });
+
+function popup() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {"message": "start"});
     });
 }
 
-// Handle the send button click event
 document.getElementById('send-button').addEventListener('click', function () {
-    const userInput = document.getElementById('user-input');
-    const message = userInput.value;
-
-    if (message) {
-        // Send the message to the content script
-        sendMessageToContentScript(message);
-        userInput.value = '';
-        
-        // Echo the message in the popup chatbox
-        const chatMessages = document.getElementById('chat-messages');
-        const userMessage = document.createElement('p');
-        userMessage.innerText = `You: ${message}`;
-        chatMessages.appendChild(userMessage);
-    }
+    document.getElementById('send-button').addEventListener("click", popup());
 });
